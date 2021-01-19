c349250a27a2fd26cf11b77dc97611657aa1ee2a
Chris Ramer <chrisramer.97@gmail.com>
2021-01-19 10:52:03 -0800

    Add code

--- ScrabbleScore.Solution/ScrabbleScore/Models/ScrabbleScore.cs
+++ ScrabbleScore.Solution/ScrabbleScore/Models/ScrabbleScore.cs
@@ -5,6 +5,39 @@ namespace Scrabble
 {
 	public class ScrabbleScore
 	{
-	
+		public string WordPlayed {get; set;}
+		Dictionary<int, List<char>> lettersWithScores = new Dictionary<int, List<char>>()
+		{
+			{ 1, new List<char>() { 'A', 'E', 'I', 'O', 'U', 'L', 'N', 'R', 'S', 'T' } },
+			{ 2, new List<char>() { 'D', 'G' } },
+			{ 3, new List<char>() { 'B', 'C', 'M', 'P' } },
+			{ 4, new List<char>() { 'F', 'H', 'V', 'W', 'Y' } },
+			{ 5, new List<char>() { 'K' } },
+			{ 8, new List<char>() { 'J', 'X' } },
+			{ 10, new List<char>() { 'Q', 'Z' } },
+		};
+
+		public ScrabbleScore(string word)
+		{
+			this.WordPlayed = word;
+		}
+
+		public int GetScore()
+		{
+			int score = 0;
+
+			for (int i = 0; i < WordPlayed.Length; i++)
+			{
+				foreach (KeyValuePair<int, List<char>> kvp in lettersWithScores)
+				{
+					for (int j = 0; j < kvp.Value.Count; j++)
+					{
+						if (kvp.Value[j] == WordPlayed[i]) score += kvp.Key;
+					}
+				}
+			}
+
+			return score;
+		}
 	}
 }
--- ScrabbleScore.Solution/ScrabbleScore/Program.cs
+++ ScrabbleScore.Solution/ScrabbleScore/Program.cs
@@ -7,7 +7,19 @@ namespace Scrabble
 	{
 		public static void Main()
 		{
+			Console.WriteLine("Give me a word and I will tell you its Scrabble score!");
 
+			string word = Console.ReadLine();
+			if (!Regex.IsMatch(word, @"^[a-zA-Z]+$"))
+			{
+				Console.WriteLine("ERROR: Must give one word in English characters.");
+				return;
+			}
+
+			ScrabbleScore scrabble = new ScrabbleScore(word.ToUpper());
+
+			Console.WriteLine("-------------------------");
+			Console.WriteLine("The word '" + word + "' scores you " + scrabble.GetScore() + " points!");
 		}
 	}
 }
